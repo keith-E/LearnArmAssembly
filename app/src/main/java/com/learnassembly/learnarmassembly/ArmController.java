4542e20a15f290b82990651c1f3032a5d049838e
Matthew Stevens <gibsonhistoric@gmail.com>
2018-11-25 17:24:54 -0500

    Started completing more methods in the ArmController class and added more to the RegisterBank class.

--- app/src/main/java/com/learnassembly/learnarmassembly/ArmController.java
+++ app/src/main/java/com/learnassembly/learnarmassembly/ArmController.java
@@ -11,7 +11,7 @@ public class ArmController {
     /*
         Initializes the ArmController.
      */
-    public ArmController(RegisterBank register, Memory mem, StackPointer, sp) {
+    public ArmController(RegisterBank register, Memory mem, StackPointer sp) {
         this.register = register;
         this.mem = mem;
         this.sp = sp;
@@ -21,13 +21,51 @@ public class ArmController {
         Mov operation for two registers.
      */
     public void mov(int rd, int rs) {
-
+        int[] cons = this.register.getRegister(rs);
+        this.register.setRegister(rd, cons);
     }
 
     /*
         Mov operation for register and constant.
      */
-    public void mov(int rd, int constant) {
+    public void mov(int rd, int[] constant) {
 
+        this.register.setRegister(rd, constant);
+    }
+
+    /*
+        Add operation.
+     */
+    public void add(int rd, int rs1, int rs2) {
+        int[] rs1Binary = this.register.getRegister(rs1);
+        int[] rs2Binary = this.register.getRegister(rs2);
+        int[] result = new int[32];
+
+        int carry = 0;  // start without carry
+
+        for (int i = rs1Binary.length - 1; i >= 0; i--) {
+            if (rs1Binary[i] == 0 && rs2Binary[i] == 0 && carry == 0)
+                result[i] = 0;
+            else if (rs1Binary[i] == 0 && rs2Binary[i] == 0 && carry == 1) {
+                result[i] = 1;
+                carry = 0;
+            } else if (((rs1Binary[i] == 0 && rs2Binary[i] == 1) ||
+                    (rs1Binary[i] == 1 && rs2Binary[i] == 0)) && carry == 0) {
+                result[i] = 1;
+            } else if (((rs1Binary[i] == 0 && rs2Binary[i] == 1) ||
+                    (rs1Binary[i] == 1 && rs2Binary[i] == 0)) && carry == 1) {
+                result[i] = 0;
+                carry = 1;
+            } else if (rs1Binary[i] == 1 && rs2Binary[i] == 1 && carry == 0) {
+                result[i] = 0;
+                carry = 1;
+            } else {
+                result[i] = 1;
+                carry = 1;
+            }
+        }
+
+        // store result in destination register
+        this.register.setRegister(rd, result);
     }
 }
--- app/src/main/java/com/learnassembly/learnarmassembly/RegisterBank.java
+++ app/src/main/java/com/learnassembly/learnarmassembly/RegisterBank.java
@@ -1,4 +1,45 @@
 package com.learnassembly.learnarmassembly;
+import java.util.ArrayList;
 
+/*
+    Represents the registers on the ARM.
+ */
 public class RegisterBank {
+    private ArrayList<int[]> rb = new ArrayList<int[]>();
+
+    /*
+        Initializes RegisterBank to contain all 0s to start.
+     */
+    public RegisterBank() {
+        for (int i = 0; i < 32; i++) {
+            int[] empty = new int[32];
+            rb.add(empty);
+        }
+    }
+
+    /*
+        Retrieve binary number at given register number.
+     */
+    public int[] getRegister(int register) {
+        try {
+            return this.rb.get(index);
+        }
+
+        catch {
+            System.err.print("Invalid register.");
+        }
+    }
+
+    /*
+        Set binary number at given register number.
+     */
+    public void setRegister(int register, int[] num) {
+        try {
+            this.rb.set(register, num);
+        }
+
+        catch {
+            System.err.print("Invalid register.");
+        }
+    }
 }
