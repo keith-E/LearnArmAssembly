0ee083003ba2161415198cf441009e95e5cd5694
Matthew Stevens <gibsonhistoric@gmail.com>
2018-11-25 11:37:20 -0500

    Starting to create general layout for ArmController class. Nothing finished yet.

--- app/src/main/java/com/learnassembly/learnarmassembly/ArmController.java
+++ app/src/main/java/com/learnassembly/learnarmassembly/ArmController.java
@@ -1,7 +1,33 @@
 package com.learnassembly.learnarmassembly;
 
+/*
+    Represents an ArmController.
+ */
 public class ArmController {
     private RegisterBank register;
     private Memory mem;
     private StackPointer sp;
+
+    /*
+        Initializes the ArmController.
+     */
+    public ArmController(RegisterBank register, Memory mem, StackPointer, sp) {
+        this.register = register;
+        this.mem = mem;
+        this.sp = sp;
+    }
+
+    /*
+        Mov operation for two registers.
+     */
+    public void mov(int rd, int rs) {
+
+    }
+
+    /*
+        Mov operation for register and constant.
+     */
+    public void mov(int rd, int constant) {
+
+    }
 }
