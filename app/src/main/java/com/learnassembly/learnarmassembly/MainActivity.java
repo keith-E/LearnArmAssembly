f260ccf635c07d14beee8e4db5266599722fb558
Matthew Stevens <gibsonhistoric@gmail.com>
2018-12-06 13:00:32 -0500

    Implemented the list!

--- app/src/main/java/com/learnassembly/learnarmassembly/MainActivity.java
+++ app/src/main/java/com/learnassembly/learnarmassembly/MainActivity.java
@@ -6,12 +6,15 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import java.io.Serializable;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
         branchButtonLogic();
     }
+
 
     private void branchButtonLogic() {
         final LinearLayout beginningButtonLayout = (LinearLayout) findViewById(R.id.begButtonLayout);
--- app/src/main/java/com/learnassembly/learnarmassembly/OperationNameActivity.java
+++ app/src/main/java/com/learnassembly/learnarmassembly/OperationNameActivity.java
@@ -2,12 +2,33 @@ package com.learnassembly.learnarmassembly;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
-public class OperationNameActivity extends AppCompatActivity {
+public class OperationNameActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_operation_name);
+
+        android.widget.Spinner spinner = findViewById(R.id.operationNameSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.operations, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
